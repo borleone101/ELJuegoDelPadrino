6c0daabd9b0155f7709c11c5d0e98b6cd31b7bca
borleone101 <borleone101@gmail.com>
2026-02-27 08:44:47 -0400

    Delete assets/js directory

--- assets/js/pagos.js
+++
@@ -1,9 +0,0 @@
-import { uploadFile } from "./cloudinary.js";
-
-document.getElementById("filePago").addEventListener("change", async (e) => {
-  const file = e.target.files[0];
-
-  const url = await uploadFile(file, "el-padrino/comprobantes");
-
-  console.log("Comprobante subido:", url);
-});
